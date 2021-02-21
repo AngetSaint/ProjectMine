187de2bbe055a8e1731f04cf2498ed4963ae9d33
Diego Guemez Anget <Diegoguemez2@hotmail.com>
2021-02-21 12:55:45 -0800

    Escenario De Prueba 1
    Primer diseño de escenario para prueba.

--- Assets/Scrips/Pickaxe.cs
+++ Assets/Scrips/Pickaxe.cs
@@ -8,7 +8,7 @@ public class Pickaxe : MonoBehaviour
     public Collider2D p_RightCollider;
     public Collider2D p_MiddleCollider;
 
-    private void OnCollisionEnter2D(Collision2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.CompareTag("Rock")){
             other.gameObject.GetComponent<RockCave>().MakeLoot();
             Destroy(other.gameObject);
